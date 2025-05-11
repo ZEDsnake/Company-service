2b780d2e6eaf75a59d2331ad5bd2ba5c60e09097
ZEDsnake <.email@example.com>
2025-05-11 16:34:49 +0300

    +docker N work version

--- config_service/src/main/java/com/zed/config_service/ConfigServiceApplication.java
+++ config_service/src/main/java/com/zed/config_service/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.zed.configservice;
+package com.zed.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
